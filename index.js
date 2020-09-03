8e45c9fb0b733fb3c281373e9c226a56c07d3699
Ramon Martinez <ramonmartinez@ramons-mbp.home>
2020-09-02 18:18:12 -0700

    changed database password

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 const { Pool } = require("pg");
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres:Janice2020@localhost:5000/ramonmartinez",
   ssl: {
     rejectUnauthorized: false,
   },
